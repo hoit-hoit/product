388a2ce7150ce9ea0bb5116763a6df3b5f0f7677
hoit-hoit <dms1dud122@naver.com>
2025-02-10 14:24:13 +0900

    update setting

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,9 +1,9 @@
 export const products = [
-  {id:1, name:'피자', price:20000,Image:'./src/assets/pizza.jpg',category:'Best'},
-  {id:2, name:'햄버거', price:8000,Image:'./src/assets/burger.jpg',category:'New'},
-  {id:3, name:'콜라', price:3000,Image:'./src/assets/cola.jpg',category:'세트'},
-  {id:4, name:'감자튀김', price:5000,Image:'./src/assets/fries.jpg',category:'음료'},
-  {id:5, name:'밀크쉐이크', price:4000,Image:'./src/assets/milkshake.jpg',category:'디저트'}
+  {id:1, name:'피자', price:20000,Image:'./public/images/pizza.jpg',category:'Best'},
+  {id:2, name:'햄버거', price:8000,Image:'./public/images/burger.jpg',category:'New'},
+  {id:3, name:'콜라', price:3000,Image:'./public/images/cola.jpg',category:'세트'},
+  {id:4, name:'감자튀김', price:5000,Image:'./public/images/fries.jpg',category:'음료'},
+  {id:5, name:'밀크쉐이크', price:4000,Image:'./public/images/milkshake.jpg',category:'디저트'}
   // {id:6, name:'불고기피자', price:4000,Image:'./src/assets/pizza.jpg',category:'Best'},
   // {id:7, name:'치즈버거', price:4000,Image:'./src/assets/burger.jpg',category:'New'},
   // {id:8, name:'제로콜라', price:4000,Image:'./src/assets/cola.jpg',category:'세트'},
